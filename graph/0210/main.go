34f2fe05ef59be1d4a74465f1edf82eb2d7a8f0f
blank <yongchun.long@pitayagames.com>
2021-05-30 18:06:58 +0800

    完成210实现

---
+++ graph/0210/main.go
@@ -0,0 +1,37 @@
+package main
+
+func main() {
+
+}
+
+func findOrder(numCourses int, prerequisites [][]int) []int {
+	degree := make([]int, numCourses)
+	edges := make([][]int, numCourses)
+	for _, pre := range prerequisites {
+		edges[pre[1]] = append(edges[pre[1]], pre[0])
+		degree[pre[0]]++
+	}
+	queue := make([]int, 0, 8)
+	for i := 0; i < numCourses;i++ {
+		if degree[i] == 0 {
+			queue = append(queue, i)
+		}
+	}
+	ans := make([]int, 0, numCourses)
+	for len(queue) > 0 {
+		v := queue[len(queue)-1]
+		ans = append(ans, v)
+		queue = queue[:len(queue)-1]
+		// 删除边
+		for _, e := range edges[v] {
+			degree[e]--
+			if degree[e] <= 0 {
+				queue = append(queue, e)
+			}
+		}
+	}
+	if len(ans) < numCourses {
+		return nil
+	}
+	return ans
+}
