d28cea5fa20a29aad25487bf8b09783a87030c53
blank <yongchun.long@pitayagames.com>
2021-06-09 13:43:36 +0800

    完成1510题实现

---
+++ dp/1510/main.go
@@ -0,0 +1,18 @@
+package main
+
+func main() {
+
+}
+
+func winnerSquareGame(n int) bool {
+	dp := make([]bool, n+1)
+	for i:=1;i<=n;i++{
+		for k := 1; k * k <= i; k++ {
+			if !dp[i-k*k] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+	return dp[n]
+}
